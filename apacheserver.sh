62121f4e4026410af855268e25420d31e65f4b0e
Toaf86 <toaf86@gmail.com>
2022-05-05 16:58:46 -0600

    apache file v2 solo funciones

--- apacheserver.sh
+++ apacheserver.sh
@@ -3,16 +3,19 @@
 clear
 echo 'SERVIDORES APACHE V1, BY TOAF86'
 echo 
-for i in "1..New Folder" "2..New File"
+for i in "1..New Folder" "2..Newindex" "3..editionfileindex" "4..confcreateconf" "5..addfitoa2enside" "6..hostsedition" "7..updateapache" "*..all"
+        
 do
     echo $i
 done
 read -p "Eleccion>>>>> " elect
+
+
+
 newfolder(){   #creacion de carpeta
    
     clear
     echo "creando numero servidor apache local"
-    echo
     echo
     echo "nombre de la carpeta de /var/www"
     read -p "Nombre Carpeta>>>>>>> " ncarpeta
@@ -20,46 +23,47 @@ newfolder(){   #creacion de carpeta
     echo "se creo la carpeta en /var/www"
     ls -la /var/www
     echo 
-    read -p "crear index (true, false)>>>> " continue
+    read -p "crear index (true, false)>>>> " next
 
-} 
+}
+
 newindex(){   #creacion de archivo index de var/www
     echo "nombre del archivo index"
     read -p "Nombre archivo index>>>>>>> " nindex
-    sudo touch /var/www/$nindex
-    echo "se creo el archivo index en /var/www/$nindex"
-    ls -la /var/www/$nindex
+    sudo touch /var/www/$ncarpeta/$nindex
+    echo "se creo el archivo index en /var/www/$ncarpeta/$nindex"
+    ls -la /var/www/$ncarpeta
     echo 
-    read -p "editar index file (true, false)>>>> " continue
+    read -p "editar index file (true, false)>>>> " next
+    } 
 
-} 
+
 editionfileindex(){#edicion de archivos index
     sudo gedit /var/www/$nindex
     echo "archivo index editado con exito"
     echo "cambios guardados"
-    read -p "crear archivo .conf (y or no)>>> " continue
+    read -p "crear archivo .conf (true, false)>>> " next
 
 }
 
 confcreateconf(){ #creacion del achivo conf
     echo "nombre del archivo conf de /etc/apache2/sites-available"
-    read -p "Nombre Carpeta>>>>>>> " fileconf
-    sudo mkdir /var/www/$fileconf
+    read -p "Nombre archivo>>>>>>> " fileconf
+    sudo touch /etc/apache2/sites-available/$fileconf
     echo "se creo el archivo .confpeta en /etc/apache2/sites-available/"
     ls -la /etc/apache2/sites-available
     echo 
-    read -p "agnadir archivo a servidor a2ensite (true, false)>>>> " continue
+    read -p "agnadir archivo ($fileconf) a servidor a2ensite (true, false)>>>> " next
 
 }
 addfitoa2enside(){ #funcin agnadir archivo con a2ensite
     echo "agnadiendo archivo con a2ensite"
+    read -p "Nombre del archivo>>>>> " fileconf
+    sudo a2ensite /etc/apache2/sites-available/$fileconf
     ls -la /etc/apache2/sites-available
+    echo "se actualizo el documento $fileconf"
     echo 
-    read -p "Nombre del archivo>>>>> " nfile
-    sudo a2ensite $nfile
-    echo "se actualizo el documento"
-    echo 
-    read -p "actualizar host (true, false)>>>> " continue
+    read -p "actualizar host (true, false)>>>> " next
 }
 hostsedition(){ #funcion para editar el host
     clear
@@ -68,7 +72,7 @@ hostsedition(){ #funcion para editar el host
     echo 
     echo "hosts actualizado"
     echo 
-    read -p "actualiar apache2 (true, false)>>>> " continue
+    read -p "actualiar apache2 (true, false)>>>> " next
 
 }
 updateapache(){ #actualizacion de apache
@@ -79,34 +83,24 @@ updateapache(){ #actualizacion de apache
     exit
 }
 
-#parte de los condicionales
-if [ $elect = 1 ]; then #folder creator and call to funtions 
+
+callfuncion(){ #llamando a todas las funciones
     newfolder
-    if [ $continue = true]; then 
-         newindex
-         if [ $continue = true ]; then 
-            editionfileindex
-             if [ $continue = true ]; then 
-                confcreateconf
-                if [ $continue = true ]; then 
-                    addfitoa2enside
-                    if [ $continue = true ]; then 
-                        hostsedition
-                        if [ $continue = true ]; then 
-                            updateapache
-                         fi
-                    fi
-                
-                fi
-             fi
-         fi
+    newindex
+    editionfileindex
+    confcreateconf
+    addfitoa2enside
+    hostsedition
+    updateapache
 
-    else
-        echo "finishing program"
-
-    fi
-
-    
+}
 
 
-fi
+
+
+
+
+#condicional zone
+
+
+
